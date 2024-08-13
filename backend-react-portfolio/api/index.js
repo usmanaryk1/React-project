51215e2232dd2fe66aa231f7570f1018ed8a9e51
usmanaryk1 <usmanaryk1@gmail.com>
2024-08-13 12:17:52 +0500

    Wraped index.js and .env file in api folder in backend

--- backend-react-portfolio/api/index.js
+++ backend-react-portfolio/api/index.js
@@ -98,6 +98,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // Export the app as a serverless function
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
