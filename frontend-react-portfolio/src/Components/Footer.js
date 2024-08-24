884d70e74f7f3793a0544f644f53a4dd13d46606
usmanaryk1 <usmanaryk1@gmail.com>
2024-08-24 16:56:34 +0500

    Added bootstrap offCanvas to handle vertical header in frontend

--- frontend-react-portfolio/src/Components/Footer.js
+++ frontend-react-portfolio/src/Components/Footer.js
@@ -1,64 +1,73 @@
 import { useEffect } from "react";
 
 const Footer = () => {
-
-    useEffect(() => {
-        const toggleBacktotop = () => {
-            const backtotop = document.querySelector('.back-to-top');
-            if (backtotop) {
-                if (window.scrollY > 100) {
-                    backtotop.classList.add('active');
-                } else {
-                    backtotop.classList.remove('active');
-                }
-            }
-        };
-
-        window.addEventListener('load', toggleBacktotop);
-        window.addEventListener('scroll', toggleBacktotop);
-
-        return () => {
-            window.removeEventListener('load', toggleBacktotop);
-            window.removeEventListener('scroll', toggleBacktotop);
-        };
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+  useEffect(() => {
+    const toggleBacktotop = () => {
+      const backtotop = document.querySelector(".back-to-top");
+      if (backtotop) {
+        if (window.scrollY > 100) {
+          backtotop.classList.add("active");
+        } else {
+          backtotop.classList.remove("active");
+        }
+      }
     };
 
-    return (
-        <>
-            {/* ======= Footer ======= */}
-            <footer id="footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <div className="copyright-box">
-                                <p className="copyright">© Copyright <strong>DevFolio</strong>. All Rights Reserved</p>
-                                <div className="credits">
-                                    {/*
+    window.addEventListener("load", toggleBacktotop);
+    window.addEventListener("scroll", toggleBacktotop);
+
+    return () => {
+      window.removeEventListener("load", toggleBacktotop);
+      window.removeEventListener("scroll", toggleBacktotop);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      {/* ======= Footer ======= */}
+      <footer id="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="copyright-box">
+                <p className="copyright">
+                  © Copyright <strong>DevFolio</strong>. All Rights Reserved
+                </p>
+                <div className="credits">
+                  {/*
         All the links in the footer should remain intact.
         You can delete the links only if you purchased the pro version.
         Licensing information: https://bootstrapmade.com/license/
         Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
       */}
-                                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  Designed by{" "}
+                  <a href="https://bootstrapmade.com/">BootstrapMade</a>
                 </div>
-            </footer>{/* End  Footer */}
-            {/* use this as future reference and use logic */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* End  Footer */}
+      {/* use this as future reference and use logic */}
 
-            {/* <div id="preloader"></div> */}
-            <a href="#home" className="back-to-top d-flex align-items-center justify-content-center" onClick={scrollToTop}><i className="bi bi-arrow-up-short"></i></a>
-        </>
-    );
-}
+      {/* <div id="preloader"></div> */}
+      <a
+        href="#home"
+        className="back-to-top d-flex align-items-center justify-content-center"
+        onClick={scrollToTop}
+      >
+        <i className="bi bi-arrow-up-short"></i>
+      </a>
+    </>
+  );
+};
 
 export default Footer;
--- frontend-react-portfolio/src/Components/Header.js
+++ frontend-react-portfolio/src/Components/Header.js
@@ -1,18 +1,171 @@
+// import { HashLink as Link } from "react-router-hash-link/dist/react-router-hash-link.cjs.production";
+// import React, { useEffect, useState } from "react";
+// import { useAuth } from "../CMSAdmin/Auth/AuthContext";
+
+// const Header = () => {
+//   const [navLinks, setNavLinks] = useState([]);
+
+//   const { user, onLogout, isAdminPage, isAuthenticated } = useAuth();
+
+//   const preventRefresh = (e) => {
+//     e.preventDefault();
+//   };
+//   console.log("header user", user);
+//   useEffect(() => {
+//     //need to change logic
+//     if (isAuthenticated && isAdminPage) {
+//       setNavLinks([
+//         { to: "/form/dashboard", label: "Dashboard" },
+//         { to: "/form/hero-form", label: "Home" },
+//         { to: "/form/about-form", label: "About" },
+//         { to: "/form/service-form", label: "Services" },
+//         { to: "/form/counter-form", label: "Counter" },
+//         { to: "/form/portfolio-form", label: "Works" },
+//         { to: "/form/testimonial-form", label: "Testimonial" },
+//         { to: "/form/certification-form", label: "Certification" },
+//         { to: "/form/contact-form", label: "Contact" },
+//         { to: "/form/social-form", label: "Social Links" },
+//       ]);
+//     } else {
+//       setNavLinks([
+//         { to: "/#hero", label: "Home" },
+//         { to: "/#about", label: "About" },
+//         { to: "/#services", label: "Services" },
+//         { to: "/#work", label: "Work" },
+//         { to: "/#certifications", label: "Certifications" },
+//         { to: "/#contact", label: "Contact" },
+//       ]);
+//     }
+//   }, [isAuthenticated, isAdminPage]);
+
+//   return (
+//     <>
+//       {/* ======= Header ======= */}
+//       <header
+//         id="header"
+//         className={`fixed-top ${
+//           isAuthenticated && isAdminPage ? "vertical-header" : ""
+//         }`}
+//       >
+//         <div className="container d-flex align-items-center justify-content-between">
+//           {!isAdminPage && (
+//             <h1 className="logo">
+//               <a href="index.html">Portfolio</a>
+//             </h1>
+//           )}
+//           {/* <h1 className="logo"><a href="index.html">Portfolio</a></h1> */}
+//           {/* Uncomment below if you prefer to use an image logo */}
+//           <nav
+//             id="navbar"
+//             className={`navbar ${
+//               isAuthenticated && isAdminPage ? "vertical-navbar" : ""
+//             }`}
+//           >
+//             {/* <a href="index.html" class="logo"><img src="assets/img/logo/png" alt="" class="img-fluid"/></a> */}
+//             <ul>
+//               {navLinks.map((link, index) => (
+//                 <li key={index}>
+//                   <Link className="nav-link" smooth to={link.to}>
+//                     {link.label}
+//                   </Link>
+//                 </li>
+//               ))}
+
+//               <li className="dropdown nav-link">
+//                 {isAuthenticated && user ? (
+//                   <>
+//                     <a href="/" onClick={preventRefresh}>
+//                       <span>{user.username}</span>
+//                       <i className="bi bi-chevron-down" />
+//                     </a>
+//                     <ul>
+//                       <li>
+//                         <Link smooth to="/" onClick={onLogout}>
+//                           LogOut
+//                         </Link>
+//                       </li>
+//                       {isAdminPage ? (
+//                         <li>
+//                           <Link smooth to="/">
+//                             Go to User Portal
+//                           </Link>
+//                         </li>
+//                       ) : (
+//                         <li>
+//                           <Link smooth to="/form/dashboard">
+//                             Go to Admin Portal
+//                           </Link>
+//                         </li>
+//                       )}
+//                     </ul>
+//                   </>
+//                 ) : (
+//                   <>
+//                     <a href="/" onClick={preventRefresh}>
+//                       <span>Register</span>
+//                       <i
+//                         className={`bi bi-chevron-down ${
+//                           user && isAdminPage ? "bi bi-chevron-right" : ""
+//                         }`}
+//                       />
+//                     </a>
+//                     <ul>
+//                       <li>
+//                         <Link smooth to="/form/login-form">
+//                           LogIn
+//                         </Link>
+//                       </li>
+//                     </ul>
+//                   </>
+//                 )}
+//               </li>
+
+//               {/* <li><Link className="nav-link  active" smooth to="/#hero">Home</Link></li>
+//                             <li><Link className="nav-link" smooth to="/#about">About</Link></li>
+//                             <li><Link className="nav-link" smooth to="/#services">Services</Link></li>
+//                             <li><Link className="nav-link" smooth to="/#work">Work</Link></li>
+//                             <li><Link className="nav-link" smooth to="/#certifications">Certifications</Link></li>
+//                             {/* <li><Link to={``}><a className="nav-link scrollto" href="#blog">Blog</a></Link></li> */}
+//               {/* <li className="dropdown"><a href="/"><span>Drop Down</span> <i className="bi bi-chevron-down" /></a>
+//                                 <ul>
+//                                     <li><a href="/">Drop Down 1</a></li>
+//                                     <li className="dropdown"><a href="/"><span>Deep Drop Down</span> <i className="bi bi-chevron-right" /></a>
+//                                         <ul>
+//                                             <li><a href="/">Deep Drop Down 1</a></li>
+//                                             <li><a href="/">Deep Drop Down 2</a></li>
+//                                             <li><a href="/">Deep Drop Down 3</a></li>
+//                                         </ul>
+//                                     </li>
+//                                 </ul>
+//                             </li>
+//                             <li><Link className="nav-link" smooth to="/#contact">Contact</Link></li>
+//                             <li><Link className="nav-link" smooth to="/form">Upload Info</Link></li> */}
+//             </ul>
+//             <i className="bi bi-list mobile-nav-toggle" />
+//           </nav>
+//           {/* .navbar */}
+//         </div>
+//       </header>
+//       {/* End Header */}
+//     </>
+//   );
+// };
+// export default Header;
+
 import { HashLink as Link } from "react-router-hash-link/dist/react-router-hash-link.cjs.production";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../CMSAdmin/Auth/AuthContext";
 
 const Header = () => {
   const [navLinks, setNavLinks] = useState([]);
-
   const { user, onLogout, isAdminPage, isAuthenticated } = useAuth();
 
   const preventRefresh = (e) => {
     e.preventDefault();
   };
-  console.log("header user", user);
+
   useEffect(() => {
-    //need to change logic
+    // Update the navLinks based on authentication and admin page status
     if (isAuthenticated && isAdminPage) {
       setNavLinks([
         { to: "/form/dashboard", label: "Dashboard" },
@@ -40,115 +193,179 @@ const Header = () => {
 
   return (
     <>
-      {/* ======= Header ======= */}
-      <header
-        id="header"
-        className={`fixed-top ${
-          isAuthenticated && isAdminPage ? "vertical-header" : ""
-        }`}
-      >
-        <div className="container d-flex align-items-center justify-content-between">
-          {!isAdminPage && (
+      {isAdminPage && isAuthenticated ? (
+        <header className="vertical-header">
+          <button
+            class="btn toggle-button"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasScrolling"
+            aria-controls="offcanvasScrolling"
+          >
+            <i
+              class="bi bi-list"
+              style={{ fontSize: "2rem", color: "black" }}
+            ></i>
+          </button>
+
+          <div
+            class="offcanvas offcanvas-start"
+            data-bs-scroll="true"
+            data-bs-backdrop="false"
+            tabindex="-1"
+            id="offcanvasScrolling"
+            aria-labelledby="offcanvasScrollingLabel"
+          >
+            <div class="offcanvas-header">
+              <h1 class="offcanvas-title" id="offcanvasScrollingLabel">
+                <a href="index.html">Portfolio</a>
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <nav id="navbar" className="vertical-navbar">
+                <a href="index.html" class="logo"></a>
+                <ul>
+                  {navLinks.map((link, index) => (
+                    <li key={index}>
+                      <Link className="nav-link" smooth to={link.to}>
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+
+                  <li className="dropdown nav-link">
+                    {isAuthenticated && user ? (
+                      <>
+                        <a href="/" onClick={preventRefresh}>
+                          <span>{user.username}</span>
+                          <i className="bi bi-chevron-down" />
+                        </a>
+                        <ul>
+                          <li>
+                            <Link smooth to="/" onClick={onLogout}>
+                              LogOut
+                            </Link>
+                          </li>
+                          {isAdminPage ? (
+                            <li>
+                              <Link smooth to="/">
+                                Go to User Portal
+                              </Link>
+                            </li>
+                          ) : (
+                            <li>
+                              <Link smooth to="/form/dashboard">
+                                Go to Admin Portal
+                              </Link>
+                            </li>
+                          )}
+                        </ul>
+                      </>
+                    ) : (
+                      <>
+                        <a href="/" onClick={preventRefresh}>
+                          <span>Register</span>
+                          <i
+                            className={`bi bi-chevron-down ${
+                              user && isAdminPage ? "bi bi-chevron-right" : ""
+                            }`}
+                          />
+                        </a>
+                        <ul>
+                          <li>
+                            <Link smooth to="/form/login-form">
+                              LogIn
+                            </Link>
+                          </li>
+                        </ul>
+                      </>
+                    )}
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+      ) : (
+        <header id="header" className="fixed-top">
+          <div className="container d-flex align-items-center justify-content-between">
             <h1 className="logo">
               <a href="index.html">Portfolio</a>
             </h1>
-          )}
-          {/* <h1 className="logo"><a href="index.html">Portfolio</a></h1> */}
-          {/* Uncomment below if you prefer to use an image logo */}
-          <nav
-            id="navbar"
-            className={`navbar ${
-              isAuthenticated && isAdminPage ? "vertical-navbar" : ""
-            }`}
-          >
-            {/* <a href="index.html" class="logo"><img src="assets/img/logo/png" alt="" class="img-fluid"/></a> */}
-            <ul>
-              {navLinks.map((link, index) => (
-                <li key={index}>
-                  <Link className="nav-link" smooth to={link.to}>
-                    {link.label}
-                  </Link>
+            <nav id="navbar" className="navbar">
+              {/* <a href="index.html" class="logo"><img src="assets/img/logo/png" alt="" class="img-fluid"/></a> */}
+              <ul>
+                {navLinks.map((link, index) => (
+                  <li key={index}>
+                    <Link className="nav-link" smooth to={link.to}>
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+
+                <li className="dropdown nav-link">
+                  {isAuthenticated && user ? (
+                    <>
+                      <a href="/" onClick={preventRefresh}>
+                        <span>{user.username}</span>
+                        <i className="bi bi-chevron-down" />
+                      </a>
+                      <ul>
+                        <li>
+                          <Link smooth to="/" onClick={onLogout}>
+                            LogOut
+                          </Link>
+                        </li>
+                        {isAdminPage ? (
+                          <li>
+                            <Link smooth to="/">
+                              Go to User Portal
+                            </Link>
+                          </li>
+                        ) : (
+                          <li>
+                            <Link smooth to="/form/dashboard">
+                              Go to Admin Portal
+                            </Link>
+                          </li>
+                        )}
+                      </ul>
+                    </>
+                  ) : (
+                    <>
+                      <a href="/" onClick={preventRefresh}>
+                        <span>Register</span>
+                        <i
+                          className={`bi bi-chevron-down ${
+                            user && isAdminPage ? "bi bi-chevron-right" : ""
+                          }`}
+                        />
+                      </a>
+                      <ul>
+                        <li>
+                          <Link smooth to="/form/login-form">
+                            LogIn
+                          </Link>
+                        </li>
+                      </ul>
+                    </>
+                  )}
                 </li>
-              ))}
-
-              <li className="dropdown nav-link">
-                {isAuthenticated && user ? (
-                  <>
-                    <a href="/" onClick={preventRefresh}>
-                      <span>{user.username}</span>
-                      <i className="bi bi-chevron-down" />
-                    </a>
-                    <ul>
-                      <li>
-                        <Link smooth to="/" onClick={onLogout}>
-                          LogOut
-                        </Link>
-                      </li>
-                      {isAdminPage ? (
-                        <li>
-                          <Link smooth to="/">
-                            Go to User Portal
-                          </Link>
-                        </li>
-                      ) : (
-                        <li>
-                          <Link smooth to="/form/dashboard">
-                            Go to Admin Portal
-                          </Link>
-                        </li>
-                      )}
-                    </ul>
-                  </>
-                ) : (
-                  <>
-                    <a href="/" onClick={preventRefresh}>
-                      <span>Register</span>
-                      <i
-                        className={`bi bi-chevron-down ${
-                          user && isAdminPage ? "bi bi-chevron-right" : ""
-                        }`}
-                      />
-                    </a>
-                    <ul>
-                      {/* <li><Link smooth to="/form/signup-form">Sign Up</Link></li> */}
-                      <li>
-                        <Link smooth to="/form/login-form">
-                          LogIn
-                        </Link>
-                      </li>
-                    </ul>
-                  </>
-                )}
-              </li>
-
-              {/* <li><Link className="nav-link  active" smooth to="/#hero">Home</Link></li>
-                            <li><Link className="nav-link" smooth to="/#about">About</Link></li>
-                            <li><Link className="nav-link" smooth to="/#services">Services</Link></li>
-                            <li><Link className="nav-link" smooth to="/#work">Work</Link></li>
-                            <li><Link className="nav-link" smooth to="/#certifications">Certifications</Link></li>
-                            {/* <li><Link to={``}><a className="nav-link scrollto" href="#blog">Blog</a></Link></li> */}
-              {/* <li className="dropdown"><a href="/"><span>Drop Down</span> <i className="bi bi-chevron-down" /></a>
-                                <ul>
-                                    <li><a href="/">Drop Down 1</a></li>
-                                    <li className="dropdown"><a href="/"><span>Deep Drop Down</span> <i className="bi bi-chevron-right" /></a>
-                                        <ul>
-                                            <li><a href="/">Deep Drop Down 1</a></li>
-                                            <li><a href="/">Deep Drop Down 2</a></li>
-                                            <li><a href="/">Deep Drop Down 3</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><Link className="nav-link" smooth to="/#contact">Contact</Link></li>
-                            <li><Link className="nav-link" smooth to="/form">Upload Info</Link></li> */}
-            </ul>
-            <i className="bi bi-list mobile-nav-toggle" />
-          </nav>
-          {/* .navbar */}
-        </div>
-      </header>
-      {/* End Header */}
+              </ul>
+              <i className="bi bi-list mobile-nav-toggle" />
+            </nav>
+            {/* .navbar */}
+          </div>
+        </header>
+      )}
     </>
   );
 };
+
 export default Header;
