b097b8b013b9ce4e8bf3813d413df683a0619c9b
usmanaryk1 <usmanaryk1@gmail.com>
2024-08-28 17:45:15 +0500

    Exported index.js as serverless function

--- backend-react-portfolio/api/index.js
+++ backend-react-portfolio/api/index.js
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
+module.exports = app;
 // const express = require("express");
 // const mongoose = require("mongoose");
 // const dotenv = require("dotenv");
