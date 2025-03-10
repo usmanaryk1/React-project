71c113239a663e3e65624e6a08d601412f22754c
usmanaryk1 <usmanaryk1@gmail.com>
2025-03-11 02:21:39 +0500

    Changd the field title to name in backend

--- backend-react-portfolio/models/dynamicSectionsSchema.js
+++ backend-react-portfolio/models/dynamicSectionsSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const dynamicSectionsSchema = new Schema({
-  title: {
+  name: {
     type: String,
     required: true,
   },
